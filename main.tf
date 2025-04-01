e083e61ae6c04d6b57b8aec5dc2e20abb865f0c5
RomanRavlyk <roman.ravlyk.ri.2023@lpnu.ua>
2025-04-01 14:51:20 +0300

    feat: DynamoDb and first 3 Lambdas

--- main.tf
+++ main.tf
@@ -9,3 +9,12 @@ module "table_courses" {
   context = module.label.context
   name = "courses"
 }
+
+module "lambda_function" {
+  source = "./modules/lambda"
+  context = module.label.context
+  courses_table     = module.table_courses.id
+  authors_table     = module.table_authors.id
+  courses_table_arn = module.table_courses.arn
+  authors_table_arn = module.table_authors.arn
+}
