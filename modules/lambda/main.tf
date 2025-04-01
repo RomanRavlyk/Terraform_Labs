0599c4175c2b87a2b3bc4323df4f3bba59bf49f2
RomanRavlyk <roman.ravlyk.ri.2023@lpnu.ua>
2025-04-01 15:41:03 +0300

    feat: added last 3 lambdas, Completed LAB1

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -110,3 +110,108 @@ module "lambda_update_course" {
 
   tags = module.label.tags
 }
+
+module "label_save_course" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "0.25.0"
+  context = var.context
+  name    = "save-course"
+}
+
+module "lambda_save_course" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "7.20.1"
+  function_name = module.label_save_course.id
+  description   = "My awesome lambda function"
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+
+  source_path = "${path.module}/src/save_course"
+
+  environment_variables = {
+    TABLE_COURSES = var.courses_table
+  }
+
+  attach_policy_statements = true
+
+  policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = ["dynamodb:PutItem"],
+      resources = ["${var.courses_table_arn}"]
+    }
+  }
+
+  tags = module.label.tags
+}
+
+module "label_delete_course" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "0.25.0"
+  context = var.context
+  name    = "delete-course"
+}
+
+module "lambda_delete_course" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "7.20.1"
+  function_name = module.label_delete_course.id
+  description   = "My awesome lambda function"
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+
+  source_path = "${path.module}/src/delete_course"
+
+  environment_variables = {
+    TABLE_COURSES = var.courses_table
+  }
+
+  attach_policy_statements = true
+
+  policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = ["dynamodb:DeleteItem"],
+      resources = ["${var.courses_table_arn}"]
+    }
+  }
+
+  tags = module.label.tags
+}
+
+module "label_get_course" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "0.25.0"
+  context = var.context
+  name    = "get-course"
+}
+
+module "lambda_get_course" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "7.20.1"
+  function_name = module.label_get_course.id
+  description   = "My awesome lambda function"
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+
+  source_path = "${path.module}/src/get_course"
+
+  environment_variables = {
+    TABLE_COURSES = var.courses_table
+  }
+
+  attach_policy_statements = true
+
+  policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = ["dynamodb:GetItem"],
+      resources = ["${var.courses_table_arn}"]
+    }
+  }
+
+  tags = module.label.tags
+}
